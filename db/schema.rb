d10e4facef6b3a2095b520521b230ff5fc9387e1
Shannon <thesqueedler@gmail.com>
2014-09-04 00:36:36 -0700

    use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140904055248) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "adjectives", force: true do |t|
     t.string "value"
   end
