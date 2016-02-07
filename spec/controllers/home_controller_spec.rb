860aaf8ffe23a4a5c1ae2808f244eef7b73b946c
Mr Rogers <jon@bunnymatic.com>
2016-02-06 18:39:37 -0800

    get more specific about errors before refactor

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -46,10 +46,10 @@ describe HomeController, type: :controller do
         Verb.create(value: 'Verb')
       end
       it 'listicle should fail because only proper nouns' do
-        expect { get 'generate', headline_type: 'listicle', format: :js }.to raise_exception
+        expect { get 'generate', headline_type: 'listicle', format: :js }.to raise_exception NoMethodError
       end
       it 'dontwanna should fail because only proper nouns' do
-        expect { post 'generate', headline_type: 'dontwanna', format: :js  }.to raise_exception
+        expect { post 'generate', headline_type: 'dontwanna', format: :js  }.to raise_exception NoMethodError
       end
       it 'works' do
         Noun.create(value: "Hat")
