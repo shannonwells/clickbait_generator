bc64ef6c24d177ad0f7271466db465bb4f470d0f
Mr Rogers <jon@bunnymatic.com>
2017-02-16 23:38:14 -0800

    Updated helper to do icons/colors without sample/cycle
    * new icons
    * new helper that includes title, icon and color for each button

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,22 +1,51 @@
+# coding: utf-8
 module HomeHelper
+  # Icons ripped from http://panmental.de/symbols/info.htm
+  # For anything fancier, we might need an icon font
+  HEADLINE_TYPES = {
+    listicle: {
+      display: "Listicle",
+      icon: "✪",
+      color: "orange"
+    },
+    whathappens: {
+      display: "Suspense",
+      icon: "⌛",
+      color: "pink"
+    },
+    dontwanna: {
+      display: "Temptation",
+      icon: "⚖",
+      color: "blue"
+    },
+    whyi: {
+      display: "Confession",
+      icon: "☠",
+      color: "green"
+    },
+    watchas: {
+      display: "Voyeurism",
+      icon: "☻",
+      color: "orange"
+    }
+  }.freeze
+
   def list_type_title(headline_type)
-    case headline_type
-      when 'listicle'
-        'Listicle'
-      when 'whathappens'
-        'Suspense'
-      when 'dontwanna'
-        "Temptation"
-      when 'whyi'
-        "Confession"
-      when 'watchas'
-        "Voyeurism"
-    end
+    HEADLINE_TYPES[headline_type.to_sym][:display]
   end
+
+  def list_type_color(headline_type)
+    HEADLINE_TYPES[headline_type.to_sym][:color]
+  end
+
+  def list_type_icon(headline_type)
+    HEADLINE_TYPES[headline_type.to_sym][:icon]
+  end
+
   def clickbait_button(button_type, headline_type)
     activeclass = button_type == headline_type ? 'active' : ''
-    color = cycle(*(%w(orange pink blue green)))
-    icon = ["♔", "♕", "♖", "♗"].sample
+    color = list_type_color(button_type)
+    icon = list_type_icon(button_type)
     title = list_type_title(button_type)
 
     button_class = ["sparkley button button--clickbait round", activeclass, color].join(" ")
