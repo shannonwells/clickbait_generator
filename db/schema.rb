61ddd15d221429df6521a385979358392ae89bd9
shannonewells <shannon@carbonfive.com>
2018-01-03 21:01:04 -0800

    migrate?

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,52 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014184753) do
+ActiveRecord::Schema.define(version: 20171020022400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "adjectives", force: :cascade do |t|
+  create_table "adjectives", id: :serial, force: :cascade do |t|
     t.string "value"
   end
 
-  create_table "clickbaits", force: :cascade do |t|
-    t.string   "headline"
-    t.string   "headline_type"
-    t.text     "tags",                       array: true
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+  create_table "clickbaits", id: :serial, force: :cascade do |t|
+    t.string "headline"
+    t.string "headline_type"
+    t.text "tags", array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
-  create_table "firsts", force: :cascade do |t|
+  create_table "firsts", id: :serial, force: :cascade do |t|
     t.string "value"
   end
 
-  create_table "nexts", force: :cascade do |t|
+  create_table "nexts", id: :serial, force: :cascade do |t|
     t.string "value"
   end
 
-  create_table "nouns", force: :cascade do |t|
-    t.string  "value"
+  create_table "nouns", id: :serial, force: :cascade do |t|
+    t.string "value"
     t.boolean "is_proper", default: false
-    t.boolean "is_agent",  default: false
+    t.boolean "is_agent", default: false
   end
 
-  create_table "particles", force: :cascade do |t|
-    t.string  "value"
+  create_table "particles", id: :serial, force: :cascade do |t|
+    t.string "value"
     t.boolean "is_singular", default: true
   end
 
-  create_table "predicates", force: :cascade do |t|
+  create_table "predicates", id: :serial, force: :cascade do |t|
     t.string "value"
   end
 
-  create_table "superlatives", force: :cascade do |t|
+  create_table "superlatives", id: :serial, force: :cascade do |t|
     t.string "value"
   end
 
-  create_table "verbs", force: :cascade do |t|
-    t.string  "value"
+  create_table "verbs", id: :serial, force: :cascade do |t|
+    t.string "value"
     t.boolean "needs_agent", default: true
   end
 
